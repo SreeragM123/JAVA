e2cdea420596250f1390876c7283f3dbf8dfb736
SreeragM123 <116860725+SreeragM123@users.noreply.github.com>
2023-03-18 21:23:38 +0530

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,20 @@
+class FibonacciSeries {
+    public static void main(String[] args){
+        while(true){
+            int n=10;
+            int a=0;
+            int b=1;
+            int c=0;
+            System.out.println(a);
+            System.out.println(b);
+            while(c<=n){
+                c=a+b;
+                System.out.println(c);
+                a=b;
+                b=c;
+            }
+            break;
+        }
+    }
+    
+}
