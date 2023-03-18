3af0427f8f02b63c38c9d54c7e0a42b03bfeeb08
SreeragM123 <116860725+SreeragM123@users.noreply.github.com>
2023-03-18 21:44:05 +0530

    Update primelim.java

--- primelim.java
+++ primelim.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 class Prime
 {
 	public static void main(String arg[])	
-	{ 
-      System.out.println("Enter a number ");
+	{
+	int i,count;
+               System.out.print("Enter n value : ");
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
-	primeCal(n);
-	}
-   static void primeCal(int num)
-   {
-	int count=0;
-	for(int i=1;i<=num;i++)
+               System.out.println("Prime numbers between 1 to "+n+" are ");
+	for(int j=2;j<=n;j++)
 	{
-	   if(num%i==0)
+	count=0;
+	for(i=1;i<=j;i++)
+	{
+	   if(j%i==0)
 	   {
-	        count++;	        
+	        count++;        
 	   }
 	}
 	if(count==2)
-	       System.out.println("prime number ");
-	else
-	System.out.println("Not a prime number ");        
-   } 
+	       System.out.print(j+"  ");     
+	}
+	}
 }
